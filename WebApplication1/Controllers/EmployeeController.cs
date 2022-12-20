4598acd551941a8f1aa89f46ef7cce41c977a519
SumeyraAltinisik <sfa2023@gmail.com>
2022-12-20 19:48:09 +0300

    update ekledik. create ekledik.

--- WebApplication1/Controllers/EmployeeController.cs
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -7,7 +7,7 @@ namespace WebApplication1.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(Repository.AllEmpoyees);
         }
         // HTTP GET VERSION
         public IActionResult Create()
@@ -21,6 +21,25 @@ namespace WebApplication1.Controllers
         {
             Repository.Create(employee);
             return View("Thanks", employee);
+        }
+
+        public IActionResult Update(string empname)
+        {
+            Employee employee = Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault();
+            return View(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Employee employee, string empname)
+        {
+            Employee e = Repository.AllEmpoyees.FirstOrDefault(b=>b.Name==empname);
+            e.Age = employee.Age;
+            e.Salary = employee.Salary;
+            e.Department = employee.Department;
+            e.Sex = employee.Sex;
+            e.Name = employee.Name;
+
+            return RedirectToAction("Index");
         }
     }
 }
